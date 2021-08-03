40d58476ec7d7eb43face98f33cbc4f30b6d2516
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-08-03 15:59:23 -0700

    8/3/21

--- week1/useInput.py
+++ week1/useInput.py
@@ -1,4 +1,7 @@
 name = input("Please provide your full name:")
-age = input("Please provide your age:")
-year = int(age)
-print("Hello " + name + " you are " + age + " years old, and will turn 100 in the year " + year)
+age = int(input("Please provide your age:"))
+year = int(input("Enter the Year:"))
+year100 = int(year) - int(age) + 100
+
+print("Hello " + name + " you are " + str(age) +
+      " years old, and will turn 100 in the year " + str(year100))

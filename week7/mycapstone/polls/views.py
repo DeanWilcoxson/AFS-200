f43f5d3f82b38a0c14b0312784711703a3dd3edc
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-09-15 22:39:18 -0700

    9/15/21

--- week7/mycapstone/polls/views.py
+++ week7/mycapstone/polls/views.py
@@ -1,9 +1,47 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
+
 
 # Create your views here.
 def hello(request):
     return HttpResponse('Hello, World. You are at the polls index.');
 
-# def home(request):
-#     return render(request, 'home.html', {'title':'Polls App'});
+
+def home(request):
+    return render(request, 'home.html', {'title': 'Polls App'})
+
+
+def form(request):
+    return render(request, 'form.html',{})
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'

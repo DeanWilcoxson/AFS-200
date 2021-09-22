d5e6a75666d793bc099a712d8e9595c727311d8b
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-09-21 20:29:51 -0700

    9/21/21

--- week7/mysite/mysite/urls.py
+++ week7/mysite/mysite/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
-# urlpatterns += [
-# ]

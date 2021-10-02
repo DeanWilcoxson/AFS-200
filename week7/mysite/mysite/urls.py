0d5755cf9ae4409f41236ebf14eb31559b7bf9f4
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2021-10-01 18:25:17 -0700

    10/1/21

--- week7/mysite/mysite/urls.py
+++ week7/mysite/mysite/urls.py
@@ -18,8 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
+    path('', include('polls.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('accounts.urls'))
 ]
-
